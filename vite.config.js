de1c26161e16a899113a578a8e2faee45efa0ecf
Jeeva <jeevav7305@gmail.com>
2026-09-23 19:31:59 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/cratign/',
+  
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
